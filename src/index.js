eb5706e71f0400f3ce5eeb1f18e3d142248cbb65
ABHISHEK KUMAR <amreshk005@gmail.com>
2021-07-22 10:04:34 +0530

    final commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 let data = [];
 let liked = [];
 let AppendCard = document.getElementById("parentContainer");
-
 function getData() {
   fetch("https://jsonplaceholder.typicode.com/users")
     .then((res) => res.json())
     .then((res) => {
       fillPage(res);
-      //   console.log(data);
+      console.log(data);
       data = res;
     });
 }
@@ -62,6 +61,8 @@ function getData() {
 //   }
 // }
 
+let modal = `  `;
+
 function fillPage(usersData) {
   //   let AppendCard = document.getElementsByClassName("container");\
   data = usersData;
@@ -70,34 +71,90 @@ function fillPage(usersData) {
     for (let i = 0; i < usersData.length; i++) {
       let { id, name, username, phone, website, email } = usersData[i];
       let ele = `
-    <div class="col-3 p-3">
-    <div class="card" style="width: 18rem;">
-        <span></span>
-        <img src=${`https://avatars.dicebear.com/v2/avataaars/${username}.svg?options%5bmood%5d%5b%5d=happy`} class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${name}</h5>
-          <p class="card-text"><span>
-             <i class="far fa-envelope"></i>
+    <div class="col-sm-12 col-md-3 col-lg-3 p-3">
+        <ow class="card" style="width: 18rem;">
+              <img src=${`https://avatars.dicebear.com/v2/avataaars/${username}.svg?options%5bmood%5d%5b%5d=happy`} class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p class="card-text">
+                    <span>
+                      <i class="far fa-envelope"></i>
                     ${email}
-               </span></p>
-          <p class="card-text"><span>
-              <i class="far fa-phone" f095></i>
-               ${phone}
-               </span></p>
-          <p class="card-text"><span>
-           <i class="far fa-globe" f095></i>
-           ${website}
-             </span></p>
-           
+                    </span>
+                </p>
+                <p class="card-text">
+                  <span>
+                    <i class="far fa-phone" f095></i>
+                    ${phone}
+                  </span>
+                </p>
+                <p class="card-text">
+                    <span>
+                      <i class="far fa-globe" f095></i>
+                    ${website}
+                    </span>
+                </p>      
+            </div>
+            <ul class="list-group list-group-horizontal">
+              <li id=${id} class="like list-group-item list-group-item-dark" id="like" style="padding: .5rem 2.4rem;" role="button">
+                  ${
+                    liked.includes(`${id}`)
+                      ? `<span id=${id} class="like">
+                        <i  id=${id} class="like far fa-heart" role="button">
+                        </i>
+                    <span>`
+                      : `<span id=${id} class="like">
+                        <i id=${id} class="like fas fa-heart">
+                        </i>
+                    <span>`
+                  }
+              </li>
+              <li id=${id} role="button" class="edit list-group-item list-group-item-dark" style="padding: .5rem 2.4rem;">
+                  <span>
+                      <i id=${id} class="edit far fa-edit" data-bs-toggle="modal" data-bs-target="#exampleModal${id}" data-bs-whatever="@mdo">
+                      </i>
+                  </span>
+              </li>
+
+              <li id=${id} class="delete list-group-item list-group-item-dark" role="button" style="padding: .5rem 2.5rem;">
+                  <i id=${id} class="delete far fa-trash">
+                  </i>
+              </li>
+          </ul>
+      <div class="modal fade" id="exampleModal${id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Basic Modal</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+              <div class="mb-3">
+                  <label><span class="text-danger">*</span>Name: </label>
+                  <input type="text" class="form-control w-75" style="display:inline;" id="name${id}" value=${name}/>
+              </div>
+              <div class="mb-3">
+                <label><span class="text-danger">*</span>Email: </label>
+                <input type="email" class="form-control w-75" style="display:inline;" id="email${id}" value=${email}>
+              </div>
+              <div class="mb-3">
+                <label><span class="text-danger">*</span>Phone: </label>
+                <input type="tel"  class="form-control w-75" style="display:inline;" id="phone${id}" value=${phone}>
+              </div>
+              <div class="mb-3">
+                <label><span class="text-danger">*</span>Website: </label>
+                <input type="url" class="form-control w-75" style="display:inline;" id="website${id}" value=${website}>
+              </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary bg-light text-dark" data-bs-dismiss="modal">Cancel</button>
+            <button type="button" class="SubmitEdit btn btn-primary" data-bs-dismiss="modal" id=${id}>Ok</button>
+          </div>
         </div>
-        <ul class="list-group list-group-horizontal">
-        <li id=${id} class="like list-group-item list-group-item-dark" id="like" style="padding: .5rem 2.4rem;">
-        ${liked.includes(id) ? `<span id=${id} class="like"><i  id=${id} class="like far fa-heart"></i><span>` : `<span id=${id} class="like"><i id=${id} class="like fas fa-heart"></i><span>`}
-        </li>
-        <li class="list-group-item list-group-item-dark" style="padding: .5rem 2.4rem;"><span><i value=${id} class="edit far fa-edit"></i></span></li>
-        <li value=${id} class="delete list-group-item list-group-item-dark" style="padding: .5rem 2.5rem;"><i value=${id} class="delete far fa-trash"></i></li>
-      </ul>
       </div>
+      </div>
+      </div>
+     
       </div>`;
       AppendCard.innerHTML += ele;
     }
@@ -110,25 +167,36 @@ getData();
 document.addEventListener(
   "click",
   function (event) {
-    console.log(event.target.id, event.target);
-
     if (event.target.classList.contains("like")) {
       // Add some logic...
-
-      if (!liked.includes(event.target.value)) {
-        liked.push(event.target.value);
+      if (!liked.includes(event.target.id)) {
+        liked.push(event.target.id);
       } else {
-        liked = liked.filter((e) => e !== event.target.value);
+        liked = liked.filter((e) => e !== event.target.id);
       }
       fillPage(data);
     }
-    if (event.target.classList.contains("edit")) {
-      // Add some logic...
-      console.log("edit", data);
+    if (event.target.classList.contains("SubmitEdit")) {
+      let updated = data.map((e) => {
+        console.log(e, e.id === Number(event.target.id));
+        if (e.id === Number(event.target.id)) {
+          let name = document.getElementById(`name${e.id}`).value;
+          let email = document.getElementById(`email${e.id}`).value;
+          let phone = document.getElementById(`phone${e.id}`).value;
+          let website = document.getElementById(`website${e.id}`).value;
+          let newObj = { ...e, name: name, email: email, phone: phone, website: website };
+          return newObj;
+        }
+        return e;
+      });
+
+      data = updated;
+      fillPage(data);
     }
     if (event.target.classList.contains("delete")) {
       // Add some logic...
-      console.log("delete", data);
+      data = data.filter((e) => e.id !== Number(event.target.id));
+      fillPage(data);
     }
   },
   false
